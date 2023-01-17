5237efa811baf4dd368917b1ce523508720d92f2
Przemyslaw Plata <paplata50@gmail.com>
2023-01-17 18:42:36 +0100

    Add generation of more pages

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fpdf import FPDF
+import pandas as pd
+
+df = pd.read_csv("topics.csv")
+
+
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
+for index,row in df.iterrows():
+    pages_num = int(row["Pages"])
+    
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.cell(w=0,h=12,txt=row["Topic"],align="L",ln=1)
+    pdf.line(10,23,200,23)
+    for _ in range(pages_num-1):
+        pdf.add_page()
+        
+    
+    
 
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0,h=12,txt="Wowowow",align="L",ln=1,border=1)
-pdf.cell(w=0,h=12,txt="Wowsadasd",align="L",ln=1,border=1)
 
-pdf.output("bruh.pdf")
+pdf.output("output.pdf")

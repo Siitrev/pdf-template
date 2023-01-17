4fdc0459ce5eccb4b62e9724775673d46f4f668f
Przemyslaw Plata <paplata50@gmail.com>
2023-01-17 19:07:58 +0100

    Code clean-up

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from fpdf import FPDF
 import pandas as pd
 
-
+# Read csv file with properties for each page
 df = pd.read_csv("topics.csv")
 
-
-
+# Generate FPDF object
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
 
+# Create certain pages
 for index,row in df.iterrows():
     pages_num = int(row["Pages"])
     
@@ -17,11 +17,9 @@ for index,row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(0,0,0)
     pdf.cell(w=0,h=12,txt=row["Topic"],align="L",ln=1)
-    pdf.line(10,20,200,20)
     
-    lines_num = (297-21)//10
-    
-    for i in range(30,297,10):
+    # Add lines
+    for i in range(20,297,10):
         pdf.line(10,i,200,i)
     
     pdf.ln(266)
@@ -38,8 +36,5 @@ for index,row in df.iterrows():
         pdf.set_text_color(100,100,100)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
         
-    
-    
-
-
+# Output the file
 pdf.output("output.pdf")
